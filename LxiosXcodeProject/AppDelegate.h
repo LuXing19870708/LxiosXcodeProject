38fdaf51599d1300601bbf1b78423395d788a3cf
LUXING <david@LUXINGdeiMac.local>
2017-03-31 09:30:35 +0800

    Initial Commit

---
+++ LxiosXcodeProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LxiosXcodeProject
+//
+//  Created by LUXING on 2017/3/31.
+//  Copyright © 2017年 LUXING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LxiosXcodeProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LxiosXcodeProject
+//
+//  Created by LUXING on 2017/3/31.
+//  Copyright © 2017年 LUXING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
